b2dba002b0aff08b79fe1aae54d410e5227bb016
huzefababinwale <hbabinwale@gmail.com>
2023-07-24 11:55:56 +0530

    Do-While

---
+++ C_Program/print_1to5.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+void main()
+{
+	int counter=1;
+	
+	do
+	{
+		printf("\n%d",counter);
+		counter=counter+1;
+	}while(counter<=5);
+}
+
+
+
+//	Step 1: Start the program;
+//	Step 2: Declare variable as counter =1 ;to store value
+//	Step 3: Print counter;
+//		3.1: counter = counter +1;
+//		3.2: check counter<=5;
+////		3.3: if Yes 
+////		3.4: go to step 3;
+////		3.5: else Go to step 4;
+//	Step 4: Stop;	
