66fac7acb915cb6302c1c3191a3918f9357d5d62
huzefababinwale <hbabinwale@gmail.com>
2023-07-24 11:30:18 +0530

    WhileLoop

---
+++ C_Program/print_Number_1to5.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+void main()
+{
+	int counter = 0;
+	
+	while (counter<=5)
+	{
+		printf("\n%d",counter);
+		counter = counter+1;
+		}	
+}
+
+
+//	Step 1: Start the program;
+//	Step 2: Declare variable as counter =1 ;to store value
+//	Step 3: check counter<=5;
+//		3.1: if Yes Print counter;
+//		3.2: counter = counter +1;
+//		3.3: go to step 3;
+//		3.4: else Go to step 4;
+//	Step 4: Stop;	
