afd1aeed2648275db5fb9e5ba524072fcade037c
huzefababinwale <hbabinwale@gmail.com>
2023-11-03 23:03:23 +0530

    constructor

---
+++ CSharp/Constructor/Constructor/Constructor/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constructor
+{
+    class Book
+    {
+        int BookId;
+        string title;
+        string author;
+        int price;
+        public Book()
+        {
+            BookId = 0;
+            title = "No_Rules";
+            author = "JRK";
+            price = 1000;
+        }
+        public void display()
+        {
+            Console.WriteLine("Book Id = " + BookId);
+            Console.WriteLine("Book Title = "+ title);
+            Console.WriteLine("Author = " + author);
+            Console.WriteLine("price = " + price);
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Book b=new Book();
+            b.display();
+            Console.ReadLine();
+        }
+    }
+}
---
+++ CSharp/Constructor/Constructor_Winform/Constructor_Winform/Product.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constructor_Winform
+{
+    internal class Product
+    {
+        public int ProductID{ get; set; }
+        public string ProductName{ get; set; }
+        public int ProductPrice{ get; set; }
+        public int ProductQty{ get; set; }
+
+        public Product()
+        {
+            ProductID = 0;
+            ProductName = "ChuaMaar";
+            ProductPrice = 20;
+            ProductQty = 7;
+        }
+        public void display()
+        {
+            Console.WriteLine("Product ID: "+ProductID);
+            Console.WriteLine("Product Name: "+ProductName);
+            Console.WriteLine("Product Price: "+ProductPrice);
+            Console.WriteLine("Product Quantity: "+ProductQty);
+            Console.WriteLine("Total Cost: "+ProductPrice*ProductQty);
+        }
+
+    }
+}
