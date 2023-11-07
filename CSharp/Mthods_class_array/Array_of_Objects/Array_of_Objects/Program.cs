36409a21437149d20290d61c11dea6f7b4e4cc6a
huzefababinwale <hbabinwale@gmail.com>
2023-11-07 12:34:09 +0530

    remake

---
+++ CSharp/Mthods_class_array/Array_of_Objects/Array_of_Objects/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Management.Instrumentation;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Array_of_Objects
+{
+    internal class Program
+    {
+        class Car
+        {
+            public string Model;
+            public Car(string Model)
+            {
+                this.Model = Model;
+            }
+            public void display()
+            {
+                Console.WriteLine(Model);
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Car[] car = new Car[3];
+
+            car[0] = new Car();
+            string[] guarage = {"Lambo","Mustang","Tesla"};
+
+            for(int i=0;i< guarage.Length;i++)
+            {
+                Console.WriteLine("Write {0} number object name",i+1);
+                
+                guarage[i]= Console.ReadLine();
+            }
+            Console.WriteLine("Saving the data Written Down Below.....");
+            for (int i = 0; i < guarage.Length; i++) 
+            {
+                Console.WriteLine(guarage[i]);
+            }
+            Array.Sort(guarage);
+
+            Console.WriteLine("Applying Sorting Algorithms........");
+            for (int i=0;i<guarage.Length; i++)
+            {
+                Console.WriteLine(guarage[i]);
+            }
+
+            
+            
+            Console.ReadKey();
+        }
+    }
+}
--- CSharp/Mthods_class_array/ConsoleApp1/ConsoleApp1/Program.cs
+++ CSharp/Mthods_class_array/ConsoleApp1/ConsoleApp1/Program.cs
@@ -8,8 +8,26 @@ namespace ConsoleApp1
 {
     internal class Program
     {
+        class Obj
+        {
+            string Name ;
+            int Number;
+        }
         static void Main(string[] args)
         {
+            Obj o1 = new Obj();
+            Obj o2= new Obj();
+            Obj o3= new Obj();
+            Obj o4= new Obj();
+            Obj o5= new Obj();
+            Obj o6= new Obj();
+            Obj o7= new Obj();
+            Obj o8= new Obj();
+            Obj o9= new Obj();
+            Obj o10= new Obj();
+
+            Array[] arr = new Array[10];
+
         }
     }
 }
