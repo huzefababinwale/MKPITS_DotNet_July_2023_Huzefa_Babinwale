cecedd56e44f48a50a40ffa5157a2012642b27cd
huzefababinwale <hbabinwale@gmail.com>
2023-11-08 16:25:30 +0530

    git_push

--- CSharp/Mthods_class_array/Array_of_Objects/Array_of_Objects/Program.cs
+++ CSharp/Mthods_class_array/Array_of_Objects/Array_of_Objects/Program.cs
@@ -12,40 +12,43 @@ namespace Array_of_Objects
         class Car
         {
             public string Model;
-            public Car(string Model)
+            public string Color;
+            public Car(string Model, string Color)
             {
                 this.Model = Model;
+                this.Color= Color;
             }
-            public void display()
+            ~Car()
             {
-                Console.WriteLine(Model);
+                Console.WriteLine("Mission Abort ");
             }
         }
 
         static void Main(string[] args)
         {
-            Car[] car = new Car[3];
-
-            car[0] = new Car();
-            string[] guarage = {"Lambo","Mustang","Tesla"};
-
+            Car[] guarage = new Car[3];
+            string col,mod;
             for(int i=0;i< guarage.Length;i++)
             {
-                Console.WriteLine("Write {0} number object name",i+1);
-                
-                guarage[i]= Console.ReadLine();
+                Console.WriteLine("Write {0} number object name and color",i+1);
+                mod=Console.ReadLine();
+                col= Console.ReadLine();
+                guarage[i] = new Car(mod, col);
             }
             Console.WriteLine("Saving the data Written Down Below.....");
             for (int i = 0; i < guarage.Length; i++) 
             {
-                Console.WriteLine(guarage[i]);
+                Console.WriteLine(guarage[i].Model);
+                Console.WriteLine(guarage[i].Color);
             }
-            Array.Sort(guarage);
-
+/*            Array.Sort(guarage);
+*/
             Console.WriteLine("Applying Sorting Algorithms........");
             for (int i=0;i<guarage.Length; i++)
             {
-                Console.WriteLine(guarage[i]);
+                Console.WriteLine(guarage[i].Model);
+                Console.WriteLine(guarage[i].Color);
+
             }
 
             
