fbba53197e2a6aa96093da41ac515b283bc3b901
huzefababinwale <hbabinwale@gmail.com>
2023-11-10 23:31:35 +0530

    staticBank

---
+++ CSharp/OOPs/BankAccount1/BankAccount/BankAccount.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BankAccount
+{
+    internal class BankAccount
+    {
+        public int BankAccountNumber {  get; set; }
+        public string BankHolderName {  get; set; }
+        public int Balance {  get; set; }
+
+        public void display()
+        {
+
+            Console.WriteLine("Bank Account Number: " + BankAccountNumber);
+        }
+    }
+
+}
