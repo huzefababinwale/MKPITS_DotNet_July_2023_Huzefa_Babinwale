053438b53203357187c4b8fe9a2e9a7452641b43
huzefababinwale <hbabinwale@gmail.com>
2023-12-02 09:35:44 +0530

    prctice

---
+++ CSharp/Windows_Form/no_upto/no_upto/Form1.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace no_upto
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            LinkedList<int> l1= new LinkedList<int>();
+            LinkedList<int> l2= new LinkedList<int>();
+            LinkedList<int> l3= new LinkedList<int>();
+            l1.AddFirst(2);
+            l1.AddLast(4);
+            l2.AddFirst(5); 
+            l2.AddLast(6);
+            int res = 0;
+            StringBuilder sb=new StringBuilder();
+            if(l1.Count ==l2.Count )
+            {
+                var node1 = l1.First;
+                var node2=l2.First;
+
+                while(node1!=null && node2!=null)
+                {
+                    res = node1.Value + node2.Value;
+                    l3.AddLast(res);
+                    node1 = node1.Next;
+                    node2=node2.Next;
+                }
+            }
+            StringBuilder sb3 = new StringBuilder();
+            foreach(int third  in l3)
+            {
+
+                sb3.Append(third+"\t");
+
+            }
+            label1.Text= sb3.ToString();
+
+
+
+            /*foreach (int l in l1)
+            {
+
+                foreach (int t in l2)
+                {
+
+                    res = l + t;
+                    l3.AddLast(res);
+                }
+            }*/
+
+
+        }
+    }
+}
---
+++ CSharp/Windows_Form/print_pattern/print_pattern/Form1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace print_pattern
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            StringBuilder sb = new StringBuilder();
+            int n=Convert.ToInt32(textBox1.Text);
+            sb.Append("n=");
+            int Square = n * n;
+            sb.AppendLine(Square+"\t");
+            int m;
+            switch (comboBox1.SelectedIndex)
+            {
+                case 0:
+                    m = n * n;
+                    break;
+                case 1:
+                    m = n;
+                    break;
+                default:
+                    m = 0; break;
+            }
+            label2.Text = m.ToString();
+            label2.Text= sb.ToString();
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            int n= Convert.ToInt32(textBox1.Text);
+            
+        }
+
+        private void webBrowser1_DocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e)
+        {
+            
+
+
+        }
+    }
+}
